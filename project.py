f05593fe75674ef76a948949a3cf90e7c55dd32c
JoshuaCooling <147016239+JoshuaCooling@users.noreply.github.com>
2023-11-02 12:10:19 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,68 @@
+import csv
+
+def fetchActorsFromDataBase():
+
+    actors = []
+
+    with open('IMDB-Movie-Data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            actors.append(row[5])
+    return actors
+
+def fetchMoviesFromDataBase():
+
+    movies = []
+
+    with open('IMDB-Movie-Data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            movies.append(row[1])
+    return movies
+
+def searchMovieByTitle(title):
+    movies = fetchMoviesFromDataBase()
+
+    for m in movies:
+        if m == title:
+            return m
+
+def searchActorsByMovie(movie):
+
+    """"
+    This function takes the input of the title of a movie
+    It searches the data for the movie title
+    Returns the names of the actors in the movie
+    """
+
+    with open('IMDB-Movie-Data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[1] == movie:
+                return row[5]
+            
+            
+def reccomneded_movies(actor):
+    """"
+    This function takes the input of the name of an actor
+    It searches the data for movies that the actor is in
+    Returns a list of movies the actors is in
+    """
+    movies = []
+
+    with open('IMDB-Movie-Data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if actor in row[5]:
+                movies.append(row[1])
+        return movies
+
+
+users = []
+
+def login():
+    username = input()
+    password = input()
+
+    if username not in users:
+        print('There is no username associated with what was entered')
