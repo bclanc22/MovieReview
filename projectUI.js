cfc4702ca8208fecc3584d30dc68e808248208c1
Dgenord17 <99490928+Dgenord17@users.noreply.github.com>
2023-11-02 12:03:14 -0400

    Update projectUI.js

--- projectUI.js
+++ projectUI.js
@@ -4,17 +4,46 @@ const movieList = document.getElementById('movieList');
 const recommendations = document.getElementById('recommendations');
 
 let movieData = [
-    { title: 'Movie 1', id: '1' },
-    { title: 'Movie 2', id: '2' },
-    { title: 'Movie 3', id: '3' },
-    { title: 'Movie 4', id: '4' },
-    { title: 'Movie 5', id: '5' },
-    { title: 'Movie 6', id: '6' },
-    { title: 'Movie 7', id: '7' },
-    { title: 'Movie 8', id: '8' },
-    { title: 'Movie 9', id: '9' },
-    { title: 'Movie 10', id: '10' }
-    // Add more movie data as needed
+    {
+        title: 'Movie 1',
+        id: '1',
+        actors: ['Actor A', 'Actor B', 'Actor C']
+    },
+    {
+        title: 'Movie 2',
+        id: '2',
+        actors: ['Actor B', 'Actor D', 'Actor E']
+    },
+    {
+        title: 'Movie 3',
+        id: '3',
+        actors: ['Actor C', 'Actor F', 'Actor G']
+    },
+    {
+        title: 'Movie 4',
+        id: '4',
+        actors: ['Actor H', 'Actor I', 'Actor J']
+    },
+    {
+        title: 'Movie 5',
+        id: '5',
+        actors: ['Actor A', 'Actor B', 'Actor K']
+    },
+    {
+        title: 'Movie 6',
+        id: '6',
+        actors: ['Actor L', 'Actor M', 'Actor N']
+    },
+    {
+        title: 'Movie 7',
+        id: '7',
+        actors: ['Actor O', 'Actor P', 'Actor Q']
+    },
+    {
+        title: 'Movie 8',
+        id: '8',
+        actors: ['Actor D', 'Actor R', 'Actor S']
+    },
 ];
 
 searchBtn.addEventListener('click', searchMovies);
@@ -39,21 +68,20 @@ function displayMovies(movies) {
         const movieItem = document.createElement('div');
         movieItem.classList.add('movie-item');
         movieItem.innerHTML = `<h2>${movie.title}</h2>`;
-        movieItem.addEventListener('click', () => showRecommendations(movie.id));
+        movieItem.addEventListener('click', () => showRecommendations(movie));
         movieList.appendChild(movieItem);
     });
 }
 
-function showRecommendations(movieId) {
-    // Simulated recommendations from the backend
-    const recommendedMovies = [
-        { title: 'Recommended Movie 1' },
-        { title: 'Recommended Movie 2' },
-        // Add more recommended movies as needed
-    ];
+function showRecommendations(selectedMovie) {
+    // Filter movies that share at least one actor with the selected movie
+    const sharedActorMovies = movieData.filter(movie => {
+        const sharedActors = selectedMovie.actors.filter(actor => movie.actors.includes(actor));
+        return sharedActors.length > 0 && movie.id !== selectedMovie.id;
+    });
 
     recommendations.innerHTML = '<h2>Recommended Movies with Similar Actors:</h2>';
-    recommendedMovies.forEach(movie => {
+    sharedActorMovies.forEach(movie => {
         const movieItem = document.createElement('div');
         movieItem.classList.add('recommended-movie');
         movieItem.innerHTML = `<h3>${movie.title}</h3>`;
