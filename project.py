6d3d21d0f3408e10c91a193a2e8e0c6062714180
JoshuaCooling <147016239+JoshuaCooling@users.noreply.github.com>
2023-11-02 14:43:09 -0400

    project.py

--- project.py
+++ project.py
@@ -1,82 +1,80 @@
 import csv
 
-def fetchActorsFromDataBase():
-    """
-    Reads the data and returns a list of actors
-    """
-    actors = []
+class MovieReccomendation:
 
-    with open('IMDB-Movie-Data.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row[5] not in actors:
-                actors.append(row[5])
-    return actors
+    def reccomneded_movies(actor):
+        """
+        This function takes the input of the name of an actor
+        It searches the data for movies that the actor is in
+        Returns a list of movies the actors is in
+        """
+        movies = []
 
-def fetchMoviesFromDataBase():
-    """
-    Reads the data and returns a list of movies
-    """
+        with open('IMDB-Movie-Data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if actor in row[5]:
+                    movies.append(row[1])
+            return movies
+        
+class MovieDataBase:
 
-    movies = []
+    def fetchActorsFromDataBase():
+        """
+        Reads the data and returns a list of actors
+        """
+        actors = []
 
-    with open('IMDB-Movie-Data.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            movies.append(row[1])
-    return movies
+        with open('IMDB-Movie-Data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[5] not in actors:
+                    actors.append(row[5])
+        return actors
 
-def searchMovieByTitle(title):
-    """
-    Searches the data for a specific movie title
-    Returns the Title, Actors, and year made
-    """
+    def fetchMoviesFromDataBase():
+        """
+        Reads the data and returns a list of movies
+        """
 
-    info = []
+        movies = []
 
-    with open('IMDB-Movie-Data.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row[1] == title:
-                info.append(row[1])
-                info.append(row[5])
-                info.append(row[6])
-
-                return info
-
-
-def searchActorsByMovie(movie):
-
-    """"
-    This function takes the input of the title of a movie
-    It searches the data for the movie title
-    Returns the names of the actors in the movie
-    """
-
-    with open('IMDB-Movie-Data.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row[1] == movie:
-                return row[5]
-            
-            
-def reccomneded_movies(actor):
-    """"
-    This function takes the input of the name of an actor
-    It searches the data for movies that the actor is in
-    Returns a list of movies the actors is in
-    """
-    movies = []
-
-    with open('IMDB-Movie-Data.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if actor in row[5]:
+        with open('IMDB-Movie-Data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
                 movies.append(row[1])
         return movies
 
+    def searchMovieByTitle(title):
+        """
+        Searches the data for a specific movie title
+        Returns the Title, Actors, and year made
+        """
 
-#print(fetchActorsFromDataBase())
-#print(fetchMoviesFromDataBase())
-#print(searchMovieByTitle("Split"))
-#print(searchActorsByMovie("Split"))
+        info = []
+
+        with open('IMDB-Movie-Data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[1] == title:
+                    info.append(row[1])
+                    info.append(row[5])
+                    info.append(row[6])
+
+                    return info
+
+
+    def searchActorsByMovie(movie):
+
+        """"
+        This function takes the input of the title of a movie
+        It searches the data for the movie title
+        Returns the names of the actors in the movie
+        """
+
+        with open('IMDB-Movie-Data.csv', 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[1] == movie:
+                    return row[5]
+                
