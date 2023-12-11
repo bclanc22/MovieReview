b602a55a657e5d335e1f2ff12a50f657cd925b95
JoshuaCooling <147016239+JoshuaCooling@users.noreply.github.com>
2023-12-11 12:27:01 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import csv
+import random
+
+app = Flask(__name__)
+CORS(app)
+
+import csv
+import random
+
+def read_csv(file_path):
+    """
+    Reads the CSV file and returns a list of dictionaries where each dictionary
+    represents a row in the CSV file.
+    """
+    with open(file_path, 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        data = [row for row in reader]
+    return data
+
+def get_movie_details_by_title(movie_title, data):
+    """
+    Takes a movie title as input and returns its details if found in the data.
+    """
+    return next((movie for movie in data if movie['Title'] == movie_title), None)
+
+def get_movies_by_shared_actors(movie_title, data):
+    """
+    Takes a movie title as input and returns a list of ten random movies that share
+    any actors with the input movie.
+    """
+    selected_movie = get_movie_details_by_title(movie_title, data)
+    if not selected_movie:
+        return None
+
+    shared_actors = selected_movie['Actors'].split(', ')
+    related_movies = [movie['Title'] for movie in data if any(actor in movie['Actors'] for actor in shared_actors)]
+    random_movies = random.sample(related_movies, min(10, len(related_movies)))
+
+    return random_movies
+
+def get_movies_by_genre(movie_title, data):
+    """
+    Takes a movie title as input and returns a list of ten random movies that share
+    at least one genre with the input movie.
+    """
+    selected_movie = get_movie_details_by_title(movie_title, data)
+    if not selected_movie:
+        return None
+
+    genres = selected_movie['Genre'].split(', ')
+    related_movies = [movie['Title'] for movie in data if any(genre.lower() in movie['Genre'].lower() for genre in genres)]
+    random_movies = random.sample(related_movies, min(10, len(related_movies)))
+
+    return random_movies
+
+def get_movies_by_director(movie_title, data):
+    """
+    Takes a movie title as input and returns a list of ten random movies that share
+    the same director as the input movie.
+    """
+    selected_movie = get_movie_details_by_title(movie_title, data)
+    if not selected_movie:
+        return None
+
+    director = selected_movie['Director']
+    related_movies = [movie['Title'] for movie in data if director.lower() in movie['Director'].lower()]
+    random_movies = random.sample(related_movies, min(10, len(related_movies)))
+
+    return random_movies
+
+@app.route('/recommendations', methods=['GET'])
+def get_recommendations():
+    movie_title = request.args.get('movieTitle')
+    data = read_csv('venv\IMDB-Movie-Data.csv')
+
+    selected_movie = get_movie_details_by_title(movie_title, data)
+    if not selected_movie:
+        return jsonify({'error': 'Movie title not found'})
+        
+    recommendations = {
+        'shared_actors': get_movies_by_shared_actors(movie_title, data),
+        'genre': get_movies_by_genre(movie_title, data),
+        'director': get_movies_by_director(movie_title, data),
+    }
+
+    return jsonify(recommendations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ frontend.test.js
@@ -0,0 +1,53 @@
+// Import the functions you want to test
+const { searchMovie, displayRecommendations } = require('C:\Users\coolj\OneDrive\Documents\CIS350\Project\frontend.html');
+// Mock XMLHttpRequest
+class MockXMLHttpRequest {
+  open(method, url) {
+    this.method = method;
+    this.url = url;
+  }
+
+  send() {
+    // Simulate sending the request
+    if (this.onreadystatechange) {
+      this.onreadystatechange();
+    }
+  }
+}
+
+// Set up a mock XMLHttpRequest
+global.XMLHttpRequest = jest.fn(() => new MockXMLHttpRequest());
+
+test('searchMovie function sends request with correct URL', () => {
+  searchMovie();
+
+  // Check if XMLHttpRequest was called with the correct URL
+  expect(global.XMLHttpRequest.mock.calls.length).toBe(1);
+  expect(global.XMLHttpRequest.mock.calls[0][0]).toEqual('GET');
+  expect(global.XMLHttpRequest.mock.calls[0][1]).toEqual('http://127.0.0.1:5000/recommendations?movieTitle=');
+});
+
+test('displayRecommendations function displays data correctly', () => {
+  // Create a mock DOM structure
+  document.body.innerHTML = `
+    <div id="recommendationList"></div>
+  `;
+
+  // Mock data to be displayed
+  const mockData = {
+    shared_actors: ['Movie 1', 'Movie 2'],
+    genre: ['Movie 3', 'Movie 4'],
+    director: ['Movie 5', 'Movie 6'],
+  };
+
+  // Call displayRecommendations function with the mock data
+  displayRecommendations(mockData);
+
+  // Get the updated DOM structure after function execution
+  const recommendationList = document.getElementById('recommendationList');
+
+  // Check if the recommendations are correctly displayed on the DOM
+  expect(recommendationList.children.length).toBe(3); // Expect 3 headings (for 'shared_actors', 'genre', 'director')
+  expect(recommendationList.children[0].tagName).toBe('H3'); // Expect the first child to be an H3 (heading)
+
+});
