a37e23ae7068ecb12c9ea5cfa23324b7c3907f0d
Devin Genord <99490928+Dgenord17@users.noreply.github.com>
2023-12-12 23:19:01 -0500

    Unit tests for backend logic

---
+++ TestMovieRecommendations.py
@@ -0,0 +1,78 @@
+import unittest
+import csv
+import MovieRecommendation
+
+class TestMovieRecommendations(unittest.TestCase):
+
+    def setUp(self):
+        self.data = MovieRecommendation.read_csv("IMDB-Movie-Data.csv")
+
+    def test_get_movie_details_by_title(self):
+        columns = MovieRecommendation.loop_through_column("Title")
+        counter = 0
+        for movie_title in columns:
+            result = MovieRecommendation.get_movie_details_by_title(movie_title, self.data)
+            self.assertEqual(result, self.data[counter])
+            counter += 1
+
+    def test_get_movies_by_shared_actors(self):
+        columns = MovieRecommendation.loop_through_column("Title")
+        for movie_title in columns:
+        #movie_title = 'Prometheus'
+            movie = MovieRecommendation.get_movie_details_by_title(movie_title, self.data)
+            recommendations = MovieRecommendation.get_movies_by_shared_actors(movie_title, self.data)
+            actors = movie['Actors'].split(', ')
+
+            # Assert that the input movie is not in the recommendations
+            self.assertNotIn(movie_title, recommendations)
+
+            if recommendations:
+                # Assert that the recommended movies share actors with the input movie
+                for recommended_movie_title in recommendations:
+                    recommended_movie = MovieRecommendation.get_movie_details_by_title(recommended_movie_title, self.data)
+                    shared_actors = recommended_movie['Actors'].split(', ')
+                    for actor in actors:
+                        if actor in shared_actors:
+                            condition = True
+                            self.assertTrue(condition)
+
+
+    def test_get_movies_by_genre(self):
+        columns = MovieRecommendation.loop_through_column("Title")
+        for movie_title in columns:
+            movie = MovieRecommendation.get_movie_details_by_title(movie_title, self.data)
+            recommendations = MovieRecommendation.get_movies_by_genre(movie_title, self.data)
+            genres = movie['Genre'].split(', ')
+
+            # Assert that the input movie is not in the recommendations
+            self.assertNotIn(movie_title, recommendations)
+
+            if recommendations:
+                # Assert that the recommended movies share at least one genre with the input movie
+                for recommended_movie_title in recommendations:
+                    recommended_movie = MovieRecommendation.get_movie_details_by_title(recommended_movie_title, self.data)
+                    shared_genres = recommended_movie['Genre'].split(', ')
+                    for genre in genres:
+                        if genre in shared_genres:
+                            condition = True
+                            self.assertTrue(condition)
+
+    def test_get_movies_by_director(self):
+        columns = MovieRecommendation.loop_through_column("Title")
+        for movie_title in columns:
+            movie = MovieRecommendation.get_movie_details_by_title(movie_title, self.data)
+            recommendations = MovieRecommendation.get_movies_by_director(movie_title, self.data)
+            director = movie['Director']
+
+            # Assert that the input movie is not in the recommendations
+            self.assertNotIn(movie_title, recommendations)
+
+            if recommendations:
+                # Assert that the recommended movies share the same director with the input movie
+                for recommended_movie_title in recommendations:
+                    recommended_movie = MovieRecommendation.get_movie_details_by_title(recommended_movie_title, self.data)
+                    recommended_movie_director = recommended_movie['Director']
+                    self.assertEqual(director,recommended_movie_director)
+
+    if __name__ == '__main__':
+        unittest.main()
